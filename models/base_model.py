61de8e1db27f01639180fcfe43b40acf35b829b0
pether20 <pether.te.z@hotmail.com>
2022-03-05 17:01:22 -0500

    Add permision of executble files

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ define basemodel"""
+
+
 from unicodedata import name
 import uuid
 from datetime import datetime
-# cuando importo models puedo acceder a todo lo que este dentro del __init__.py
+#importo models puedo acceder a todo lo que este dentro del __init__.py
 import models
 # format date
 fd = "%Y-%m-%dT%H:%M:%S.%f"
@@ -13,8 +15,7 @@ class BaseModel:
     """ class BaseModel """
 
     def __init__(self, *args, **kwargs):
-        """ self  """
-        # Aqui compruebo si kwargs existe, sino else
+        """Methode init(self)  """
         if kwargs:
             for key, val in kwargs.items():
                 if key != '__class__':
@@ -27,15 +28,16 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
             models.storage.new(self)
 
     def save(self):
-        """ save """
+        """Methode save """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ to dict"""
+        """Methode to dict"""
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
         dict["created_at"] = self.created_at.isoformat()
@@ -43,6 +45,6 @@ class BaseModel:
         return dict
 
     def __str__(self):
-        """ str """
+        """ Methode str """
         name = BaseModel.__name__
         return ("[{}] ({}) {}".format(name, self.id, self.__dict__))
