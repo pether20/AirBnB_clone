616e3117359390ce2eab6f98506fb5b77e5c16bd
pether20 <pether.te.z@hotmail.com>
2022-03-05 21:36:02 -0500

    Add Style and coment Class

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python3
 """
-Class BaseModel for Airbnb
+Class BaseModel
 """
-import uuid
+
 from datetime import datetime
+import uuid
 import models
-# format date
-fd = "%Y-%m-%dT%H:%M:%S.%f"
+# dtm = date format
+dtm = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """
-    Class BaseModel
-    """
+    """Base Model"""
+
     def __init__(self, *args, **kwargs):
-        """Methode init_self"""
+        """Initialize a BaseModel"""
         if kwargs:
             for key, val in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, val)
-            if type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], fd)
-            if type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], fd)
+            if hasattr(self, 'created_at') and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], dtm)
+            if hasattr(self, 'updated_at') and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], dtm)
 
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
+    def __str__(self):
+        """str representation"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
     def save(self):
-        """Methode save """
+        """updates the public ins attr upd_at with the curren one"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Methode to dict"""
-        dict = self.__dict__.copy()
-        dict["__class__"] = self.__class__.__name__
-        dict["created_at"] = self.created_at.isoformat()
-        dict["updated_at"] = self.updated_at.isoformat()
-        return dict
-
-    def __str__(self):
-        """Methode str """
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        """ returns a dic containing keys and values of the instance"""
+        n_dict = self.__dict__.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"].strftime(dtm)
+        if "updated_at" in n_dict:
+            n_dict["updated_at"] = n_dict["updated_at"].strftime(dtm)
+        n_dict["__class__"] = self.__class__.__name__
+        return n_dict
